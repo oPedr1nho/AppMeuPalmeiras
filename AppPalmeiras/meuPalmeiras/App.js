c024c630a2b9e3d193e7ed954568ed84a596b76d
oPedr1nho <124839157+oPedr1nho@users.noreply.github.com>
2024-05-28 22:17:21 -0300

    Add files via upload

--- AppPalmeiras/meuPalmeiras/App.js
+++ AppPalmeiras/meuPalmeiras/App.js
@@ -1,76 +1,77 @@
-import { useFonts, 
-         Kanit_200ExtraLight,
-         Kanit_600SemiBold} from '@expo-google-fonts/kanit';
-import {View} from "react-native";
+import React from 'react';
+import { useFonts, Montserrat_400Regular, Montserrat_600SemiBold } from '@expo-google-fonts/montserrat';
+import { View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Produto from './src/telas/Produtos'
+import Filmes from './src/telas/Camisas'
 import Sobre from './src/telas/Sobre'
+import Produto from './src/telas/Produtos'
 import mock from './src/mocks/produto'
 import mock_sobre from './src/mocks/sobre'
+import mock_filmes from './src/mocks/filmes'
 
-function MenuKit(){
-  return <Produto {...mock}/>
+function MenuKit() {
+  return <Produto {...mock} />
 }
 
-function MenuSobre(){
-  return <Sobre {...mock_sobre}/>
+function MenuFilmes() {
+  return <Filmes {...mock_filmes} />;
+}
+
+function MenuSobre() {
+  return <Sobre {...mock_sobre} />;
 }
 
 const Tab = createBottomTabNavigator();
 
-function TabsMenu(){
-  return <Tab.Navigator 
-            screenOptions={({route}) => ({
-              tabBarIcon: ({focused, color, size}) => {
-                let iconName;
+function TabsMenu() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-                if(route.name === "Kit"){
-                  iconName = focused
-                  ? 'basket'
-                  : 'basket-outline';
-                } else if (route.name === "Sobre nós"){
-                  iconName = focused
-                  ? 'information-circle'
-                  : 'information-circle-outline';
-                } else if (route.name === "Produtos") {
-                  iconName = focused
-                  ? 'list'
-                  : 'list-outline';
-                } else if (route.name === "Lista de Desejos"){
-                  iconName = focused
-                  ? 'heart'
-                  : 'heart-outline';
-                }
+          if (route.name === 'PACOTE') {
+            iconName = focused ? 'cube' : 'cube-outline';
+          } else if (route.name === 'SOBRE') {
+            iconName = focused ? 'alert-circle' : 'alert-circle-outline';
+          } else if (route.name === 'CAMISETAS') {
+            iconName = focused ? 'shirt' : 'shirt-outline';
+          } else if (route.name === 'DESEJOS') {
+            iconName = focused ? 'cart' : 'cart-outline';
+          }
 
-                return <Ionicons name={iconName} size={size} color={color}/>
-              },
-              tabBarActiveTintColor: 'green',
-              tabBarInactiveTintColor: 'green',
-              tabBarHideOnKeyboard: true,
-              headerShown: false,
-            })}>
-              <Tab.Screen name="Sobre nós" component={MenuSobre}/>
-              <Tab.Screen name="Kit" component={MenuKit} />
-              <Tab.Screen name="Produtos" component={MenuKit}/>
-              <Tab.Screen name="Lista de Desejos" component={MenuKit}/>
-            </Tab.Navigator>
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#586e26',
+        tabBarInactiveTintColor: '#586e26',
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="SOBRE" component={MenuSobre} />
+      <Tab.Screen name="PACOTE" component={MenuKit} />
+      <Tab.Screen name="CAMISETAS" component={MenuFilmes} />
+      <Tab.Screen name="DESEJOS" component={MenuKit} />
+    </Tab.Navigator>
+  );
 }
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratSemiBold: Montserrat_600SemiBold,
+  });
 
-  //Carrega a fonte para dentro do projeto
-  const [ fonteCarregada ] = useFonts ({"KanitRegular" : Kanit_200ExtraLight, 
-                                        "KanitBold" : Kanit_600SemiBold});
-
-  //Se a fonte não tiver sido carregada, não exibe o projeto
-  if(!fonteCarregada) {
-    return <View />
+  if (!fonteCarregada) {
+    return <View />;
   }
-  
-  return <NavigationContainer >
-            <TabsMenu />
-        </NavigationContainer>
+
+  return (
+    <NavigationContainer>
+      <TabsMenu />
+    </NavigationContainer>
+  );
 }
--- AppPalmeiras/meuPalmeiras/src/mocks/sobre.js
+++ AppPalmeiras/meuPalmeiras/src/mocks/sobre.js
@@ -5,7 +5,7 @@ const sobre = {
     textos: {
         logo: Logo,
         Icon: Icon,
-        historia: "Palmeiras Store a loja oficial do Palmeiras na internet. Muito mais que uma loja online, somos o reduto oficial da torcida que canta e vibra. Aqui você encontra uniformes de jogo, coleções exclusivas, linhas casuais, artigos para presentes, entre outros. Tudo com condições especiais de pagamento. Ao adquirir qualquer produto na nossa loja, você estará contribuindo diretamente para o futebol do Verdão. Vista as cores do nosso Palestra. Compre na Palmeiras Store. \n\nVeja como funciona a produção:"
+        historia: "Palmeiras Store a loja oficial do Palmeiras na internet. Muito mais que uma loja online, somos o reduto oficial da torcida que canta e vibra. Aqui você encontra uniformes de jogo, coleções exclusivas, linhas casuais, artigos para presentes, entre outros. Tudo com condições especiais de pagamento. Ao adquirir qualquer produto na nossa loja, você estará contribuindo diretamente para o futebol do Verdão. Vista as cores do nosso Palestra. Compre na Palmeiras Store. \n\n"
     } 
 }
 
